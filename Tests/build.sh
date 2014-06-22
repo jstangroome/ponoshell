328473452c24f45cde5286c30d682d1f58d5c52b
Jason Stangroome <jason@codeassassin.com>
2014-06-23 07:32:13 +1000

    Fix test execution

--- Tests/build.sh
+++ Tests/build.sh
@@ -9,8 +9,10 @@ MONO_BIN=/opt/mono/bin
 
 MCS=$MONO_BIN/mcs
 
+cp $SCRIPTROOT/../packages/NUnit.2.6.3/lib/nunit.framework.dll .
 $MCS -out:Tests.dll -platform:x64 -target:library -reference:$SCRIPTROOT/../packages/NUnit.2.6.3/lib/nunit.framework.dll $SCRIPTROOT/MyClass.cs
 
 MONO=$MONO_BIN/mono
 
-$MONO --runtime=v4.0 $SCRIPTROOT/../tools/NUnit-2.6.3/bin/nunit-console.exe Tests.dll
+NUNIT_CONSOLE=`readlink --canonicalize $SCRIPTROOT/../tools/NUnit-2.6.3/bin/nunit-console.exe`
+$MONO --runtime=v4.0 $NUNIT_CONSOLE Tests.dll
