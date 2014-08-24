4f9784aa48fa406f4cb9a6170570ed98eeb0b9ba
Jason Stangroome <jason@codeassassin.com>
2014-08-24 15:54:01 +1000

    We need runtime v4 because the Guid format bug is still present in v2

--- Tests/build.sh
+++ Tests/build.sh
@@ -14,4 +14,5 @@ cp --verbose $SCRIPTROOT/../libkernel32/bin/libkernel32.so bin/
 $XBUILD $SCRIPTROOT/Tests.csproj || exit 1
 
 NUNIT_CONSOLE=`readlink --canonicalize $SCRIPTROOT/../tools/NUnit-2.6.3/bin/nunit-console.exe`
+# --runtime-v4.0 is to deal with the Guid X format bug being only half-fixed
 MONO_REGISTRY_PATH=$SCRIPTROOT/../registry $MONO --runtime=v4.0 $NUNIT_CONSOLE $SCRIPTROOT/bin/Tests.dll
